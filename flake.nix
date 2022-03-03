32fae346712506ef289ee6707dfcbe3274d680f9
Michael Davis <mcarsondavis@gmail.com>
2022-03-03 14:01:45 -0600

    add check for formatting

--- flake.nix
+++ flake.nix
@@ -1,5 +1,17 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        checks = {
+          format = pkgs.runCommand "format-check" { } ''
+            ${pkgs.nixfmt}/bin/nixfmt --check ${./.}/**.nix
+          '';
+        };
+      });
 }
